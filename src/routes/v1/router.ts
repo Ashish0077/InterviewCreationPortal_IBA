e1d16c6c11480ed50489636e247e65065ac5b708
Ashish Arora <ashish007722@gmail.com>
2021-06-19 14:47:59 +0530

    added interviews route
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- src/routes/v1/router.ts
+++ src/routes/v1/router.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import interviews from "./interview/interviews";
 import participants from "./participant/participants";
 
 const router = express.Router();
 
 router.use("/participants", participants);
+router.use("/interviews", interviews)
 
 export default router;
