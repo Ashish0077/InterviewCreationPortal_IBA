1b062f1db60595386bbea73eed964ba35ee1cc2c
Ashish Arora <ashish007722@gmail.com>
2021-06-19 07:02:10 +0530

    DB connection setup and logging setup - using morgan for logging - added ormconfig file for mysql db connection using typeorm
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
+import morgan from "morgan";
+import connectDB from "./database/db";
+
+connectDB();
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/test", (req: Request, res: Response, next: NextFunction) => {
     res.json({
