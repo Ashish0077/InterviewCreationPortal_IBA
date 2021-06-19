895f0b3bcc14e633d402bb41511ef772faef7b62
Ashish Arora <ashish007722@gmail.com>
2021-06-19 21:28:58 +0530

    [3RE Architecture] ApiErrorHandler - api error handler implementation for 3RE Architecture - all the errors will be handled by this class
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { NextFunction, Request, Response } from "express";
 import morgan from "morgan";
 import connectDB from "./database/db";
 import routerV1 from "./routes/v1/router";
+import { ApiError, InternalError } from "./core/ApiError";
+import { environment } from "./config";
 
 connectDB();
 
@@ -17,6 +19,19 @@ app.get("/test", (req: Request, res: Response, next: NextFunction) => {
 		success: true,
 		msg: "Server is up and running. Voila!!"
 	});
+});
+
+// Middleware Error Handler
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	// redirecting the error to ApiError Class
+	if (err instanceof ApiError) {
+		ApiError.handle(err, res);
+	} else {
+		if (environment == "dev") {
+			return res.status(500).send(err.message);
+		}
+		ApiError.handle(new InternalError(), res);
+	}
 });
 
 export default app;
