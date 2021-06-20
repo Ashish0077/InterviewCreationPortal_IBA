31cfc2e9f918c7a31078d78ad250e8a1ab704334
Ashish Arora <ashish007722@gmail.com>
2021-06-20 14:44:33 +0530

    Seeder updated
    Signed-off-by: Ashish Arora <ashish007722@gmail.com>

--- src/utils/seeder.ts
+++ src/utils/seeder.ts
@@ -14,43 +14,43 @@ const seeder = async () => {
 			email: "ashish007722@gmail.com"
 		},
 		{
+			name: "Kyle Simpson",
+			role: ParticipantRole.CANDIDATE,
+			email: "kyleSimpson@email.com"
+		},
+		{
+			name: "Will Sentence",
+			role: ParticipantRole.CANDIDATE,
+			email: "willsentence@email.com"
+		},
+		{
+			name: "Nancy Winters",
+			role: ParticipantRole.CANDIDATE,
+			email: "nancywinters@email.com"
+		},
+		{
 			name: "Abhishek Tiwari",
 			role: ParticipantRole.INTERVIEWER,
 			email: "abhishek@interviewbit.com"
 		},
 		{
-			name: "Bhumika Arora",
-			role: ParticipantRole.CANDIDATE,
-			email: "bhumikaarora0077@gmail.com"
+			name: "Mark Rober",
+			role: ParticipantRole.INTERVIEWER,
+			email: "markrober@email.com"
 		},
 		{
-			name: "Mohak Kapoor",
+			name: "Mohak kapoor",
 			role: ParticipantRole.INTERVIEWER,
-			email: "mohak@interviewbit.com"
+			email: "mohakkapoor@email.com"
+		},		
+		{
+			name: "Neha Singh",
+			role: ParticipantRole.INTERVIEWER,
+			email: "nehasingh@email.com"
 		}
 	];
 	await pRepo.query("delete from participants");
-	await iRepo.query("delete from interviews");
 	await pRepo.save(participants);
-	const p = await pRepo.find();
-	const interviews: IInterview[] = [
-		{
-			startTime: new Date("2021-07-01T11:30:00.000Z"),
-			endTime: new Date("2021-07-01T12:30:00.000Z"),
-			participants: [p[0], p[1]]
-		},
-		{
-			startTime: new Date(2021, 6, 2, 17, 0),
-			endTime: new Date(2021, 6, 3, 18, 0),
-			participants: [p[2], p[3]]
-		},
-		{
-			startTime: new Date(2020, 6, 2, 17, 0),
-			endTime: new Date(2020, 6, 1, 18, 0),
-			participants: [p[1], p[3]]
-		}
-	];
-	await iRepo.save(interviews);
 };
 
 export default seeder;
